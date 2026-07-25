6e6adeb4b08c21c6adc3986a7c48f423c2d055d4
Sophie <sophie.thi.nguyen11@gmail.com>
2026-07-24 18:48:55 -0700

    exports and all thats too

---
+++ scripts/ui/ui.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+@onready var battery_label: RichTextLabel = $BatteryLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
