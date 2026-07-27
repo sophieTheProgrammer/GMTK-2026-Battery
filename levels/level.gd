b2622456ff3dcfdb940a2dcb304d463e8c076008
Sophie <sophie.thi.nguyen11@gmail.com>
2026-07-26 17:53:23 -0700

    load_level in level manager

--- levels/level.gd
+++ levels/level.gd
@@ -5,38 +5,3 @@ const LEVEL_PATH : String = "res://levels/level_"
 func _ready() -> void:
 	#EventBus.phone_enter_charger.connect(_on_phone_enter_charger)
 	self.position = Vector2.ZERO
-	
-#func _on_phone_enter_charger(area : Area2D) -> void:
-	#EventBus.level_completed.emit()
-	'''
-	var next_level_number : int = self.name.to_int() + 1
-	
-	var next_level_path : String = LEVEL_PATH + str(next_level_number) + ".tscn"
-	if !load(next_level_path):
-		print("no more levels to load", next_level_path, "name", name)
-		return
-	
-	var next_level : Node = load(next_level_path).instantiate()
-	next_level.name = "level" + str(next_level_number)
-	next_level.add_to_group("level")
-
-	await Global.fade_node.fade(1.5).finished
-	print("Finished Level Fade await")
-	get_tree().current_scene.call_deferred("add_child", next_level)
-	print("instaniated level", next_level_number)
-	Global.fade_node.fade(0)
-	# TODO: add fancy smanchy scene transitions
-	call_deferred("queue_free")
-	'''
-func get_next_level_path() -> String:
-	var node_name : String = get_tree().get_first_node_in_group("level").name
-	var next_level_number : int = node_name.to_int() + 1
-	var current_level_number : String = str(node_name.to_int())
-	
-	
-	var next_level_path : String = LEVEL_PATH + str(next_level_number) + ".tscn"
-	if !next_level_path:
-		print("no more levels to load")
-		return current_level_number
-	return next_level_path
-	
--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -4,20 +4,32 @@ extends Node2D
 var current_level_index : int = 0
 var current_charger : Charger
 var current_level : Node
-func _ready() -> void:	
-	var first_level : Node = levels[current_level_index].instantiate()
-	add_child(first_level)
-	
-	current_charger = first_level.get_node("charger")
-	current_charger.phone_enter_charger.connect(_on_phone_enter_charger)
-	current_level = first_level
+
+
+func _ready() -> void:
+	_load_level(0)
+
+
 func _on_phone_enter_charger(area: Area2D) -> void:
 	EventBus.start_next_level.emit()
 	print("emitted start_next_level")
 	# Start new level here
-	current_level.queue_free.call_deferred()
-	
 	current_level_index += 1
-	var next_level : Node = levels[current_level_index].instantiate()
-	add_child.call_deferred(next_level)
-	current_level = next_level
+	_load_level(current_level_index)
+
+
+func _load_level(level_number : int) -> void:
+	var level : Node
+	
+	if current_level:
+		current_level.queue_free.call_deferred()
+	if levels[current_level_index]:
+		level = levels[current_level_index].instantiate()
+		add_child.call_deferred(level)
+	else:
+		printerr("no more levels")
+	
+	# get charger reference here
+	current_charger = level.get_node("charger")
+	current_charger.phone_enter_charger.connect(_on_phone_enter_charger)
+	current_level = level
