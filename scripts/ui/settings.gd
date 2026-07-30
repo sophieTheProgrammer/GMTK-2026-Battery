28448b7489107622157bf02ece3b712eeaa4cc01
Sophie <sophie.thi.nguyen11@gmail.com>
2026-07-29 17:38:35 -0700

    start screen yayy

--- scripts/ui/settings.gd
+++ scripts/ui/settings.gd
@@ -1,14 +1,6 @@
-extends Node2D
+extends Control
 
 @onready var back_button: Button = $BackButton
-
-func _button_pressed(source: BaseButton) -> void:
-	if source == back_button:
-		get_tree().change_scene_to_packed(load("res://scenes/start.tscn"))
-
-
-func _on_button_3_pressed() -> void:
-	AudioPlayer.play_sfx(AudioPlayer.CLICK)
 
 func _on_music_slider_value_changed(value: float) -> void:
 	var index : int = AudioServer.get_bus_index("Music")
@@ -17,3 +9,7 @@ func _on_music_slider_value_changed(value: float) -> void:
 func _on_sfx_slider_value_changed(value: float) -> void:
 	var index : int = AudioServer.get_bus_index("SFX")
 	AudioServer.set_bus_volume_db(index, linear_to_db(value))
+
+
+func _on_back_button_pressed() -> void:
+	hide()
--- scripts/ui/start_screen.gd
+++ scripts/ui/start_screen.gd
@@ -1,16 +1,28 @@
 extends Node2D
+@onready var start_screen_start_button: Button = $CanvasLayer/MarginContainer/VBoxContainer/StartScreenStartButton
+@onready var start_screen_settings_button: Button = $CanvasLayer/MarginContainer/VBoxContainer/StartScreenSettingsButton
 
-@onready var start_screen_tutorial_button: Button = $CanvasLayer/StartScreenTutorialButton
-@onready var start_screen_start_button: Button = $CanvasLayer/StartScreenStartButton
-@onready var start_screen_settings_button: Button = $CanvasLayer/StartScreenSettingsButton
+@onready var start_title: Label = $CanvasLayer/VBoxContainer/StartTitle
+@onready var settings: Control = $CanvasLayer/MarginContainer/Settings
+@onready var phone_sprite: AnimatedSprite2D = $PhoneSprite
 
 func _button_pressed(source: BaseButton) -> void:
 	match source:
-		start_screen_tutorial_button:
-			get_tree().change_scene_to_packed(Global.TUTORIAL)
 		start_screen_start_button:
 			get_tree().change_scene_to_packed(Global.GAME)
-		start_screen_start_button:
-			get_tree().change_scene_to_packed(Global.SETTINGS)
+		start_screen_settings_button:
+			settings.show()
 
 	AudioPlayer.play_sfx(AudioPlayer.CLICK, 0.25)
+func _ready() -> void:
+	float_up()
+	settings.hide()
+
+func float_up() -> void:
+	var tween : Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(phone_sprite, "position:y", phone_sprite.position.y - 20, 1)
+	tween.tween_callback(float_down)
+func float_down() -> void:
+	var tween : Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(phone_sprite, "position:y", phone_sprite.position.y + 20, 1)
+	tween.tween_callback(float_up)
