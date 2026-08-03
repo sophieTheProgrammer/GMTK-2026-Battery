1cd4274420c6fc3c9ac16a266451b16a69388f63
Sophie <sophie.thi.nguyen11@gmail.com>
2026-08-03 15:49:16 -0700

    YEAH FUNCTIONALITY FOR MULTIPLE CHARGERS THAT IS UTTERLY USELESS YAY!!!!!

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var levels: Array[PackedScene]
 var current_level_index : int = 0
-var current_charger : Charger
+#var current_charger : Charger
 var current_level : Node
 
 func _ready() -> void:
@@ -32,16 +32,17 @@ func _load_level(level_number : int) -> void:
 
 	if levels[level_number]:
 		level = levels[level_number].instantiate()
-		add_child.call_deferred(level)
+		add_child(level)
 	else:
 		printerr("no more levels")
-	
-	# get charger reference here
-	for chargers in level.get_nodes_in_group("group_name")
-	current_charger = level.get_node("charger")
-	current_charger.phone_recieved.connect(_on_phone_recieved)
 	current_level = level
-	if !current_charger:
-		print("NO CHARGER REFERENCE FROM LEVEL_MANAGER.GD")
-	else:
-		print(current_charger)
+	_connect_charger_signals.call_deferred()
+
+func _connect_charger_signals() -> void:
+	var chargers : Array = get_tree().get_nodes_in_group("charger")
+	if !chargers:
+		print("no charger reference in this level")
+	
+	for charger : Charger in chargers:
+		print("connecting charger")
+		charger.phone_recieved.connect(_on_phone_recieved)
