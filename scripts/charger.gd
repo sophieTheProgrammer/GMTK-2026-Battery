0c5d450c4f3d997a9854025d88ba9a30e8170f53
Sophie <sophie.thi.nguyen11@gmail.com>
2026-08-03 15:29:04 -0700

    working?? commit

--- scripts/charger.gd
+++ scripts/charger.gd
@@ -5,28 +5,19 @@ signal phone_recieved
 
 # WARNING: CHARGER ONLY WORKS FACING DOWN
 '''
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("phone"):
-		if Global.DEBUG_EASY_WIN or body.velocity.y < 0:
-			print("phone entered charger")
-			EventBus.phone_enter_charger.emit(self)
-			print("finished emitted phone_enter_charger")
-			set_deferred("monitoring", false)
-'''
-func _on_area_entered(area: Area2D) -> void:
+func _on_area_entered(area: Area2D) -> void:	
 	if Global.DEBUG_EASY_WIN or area.get_parent().velocity.y < 0:
 		phone_recieved.emit(self)
+'''
 
-
-func _ready() -> void:
-
-	#var entry_vector : Vector2 = global_position.direction_to(body.global_position)
-	#print(entry_vector)
-	pass
-	# if it entred the range of 180
-	#print(entry_vector.dot(Vector2.UP))
-	#if entry_vector.dot(Vector2.UP) > 0:
-		#print("entered charger properly")
-	#if body.is_in_group("phone"):
-	#	print("Player detected in charger area")
-		#EventBus.phone_enter_charger.emit(self)
+func _on_area_entered(area: Area2D) -> void:
+	#phone_recieved.emit(self)
+	var entry_vector : Vector2 = global_position.direction_to(area.global_position)
+	print(entry_vector)
+	if area.get_parent().is_in_group("phone"):
+		print("RAAAWRYGIYAWGKDHSAKHDJ A PHONE ENTERED ME!!!")
+		#print((entry_vector.dot(Vector2.UP)))
+		phone_recieved.emit(self)
+		if entry_vector.dot(Vector2.UP) > 0:
+			print("Player detected in charger area")
+			
--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -11,6 +11,7 @@ func _ready() -> void:
 
 # listen to signal from child to start new level here
 func _on_phone_recieved(area: Area2D) -> void:
+	print("phone recieved from levelmanager!!")
 	# Start new level here
 	current_level_index += 1
 	# we can do additional checks if the level was really completed here
@@ -36,6 +37,11 @@ func _load_level(level_number : int) -> void:
 		printerr("no more levels")
 	
 	# get charger reference here
+	for chargers in level.get_nodes_in_group("group_name")
 	current_charger = level.get_node("charger")
 	current_charger.phone_recieved.connect(_on_phone_recieved)
 	current_level = level
+	if !current_charger:
+		print("NO CHARGER REFERENCE FROM LEVEL_MANAGER.GD")
+	else:
+		print(current_charger)
